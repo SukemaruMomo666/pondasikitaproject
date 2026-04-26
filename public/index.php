500cc51052fa4f8b6f39e58938f7d6998b2d37b8
sauva20 <2qistysauva@gmail.com>
2026-04-26 21:54:30 +0700

    vlc

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 
+// Tambahkan kode ini:
+$_SERVER['REQUEST_URI'] = str_replace('/pondasikita', '', $_SERVER['REQUEST_URI']);
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
